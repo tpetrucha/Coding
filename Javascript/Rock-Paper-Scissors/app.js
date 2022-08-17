ce058053ceed376ed0f5df6a592d3c97cea498a2
Thomas Petrucha <t.petrucha@gmail.com>
2022-08-17 13:30:22 +0200

    normal version ready

--- Javascript/Rock-Paper-Scissors/app.js
+++ Javascript/Rock-Paper-Scissors/app.js
@@ -2,27 +2,57 @@ const computerChoiceDisplay = document.getElementById("computer-choice");
 const userChoiceDisplay = document.getElementById("user-choice");
 const resultDisplay = document.getElementById("result");
 const possibleChoices = document.querySelectorAll("button");
-const choices = ['rock','paper','scissors'];
+const choices = ["rock", "paper", "scissors"];
+const results = ["you win!", "you lost!", "it´s a draw!"];
 let userChoice;
 let computerChoice;
 let result;
+computerPoints = 0;
+userPoints = 0;
 
 possibleChoices.forEach((possibleChoice) =>
   possibleChoice.addEventListener("click", (e) => {
     userChoice = e.target.id;
     userChoiceDisplay.innerHTML = userChoice;
-    generateComputerChoice()
+    generateComputerChoice();
+    setResult();
   })
 );
 
-function generateComputerChoice(){
-    const randomNumber = Math.floor(Math.random() * possibleChoices.length)   // or * 3
-    computerChoice = choices[randomNumber];
-    computerChoiceDisplay.innerHTML = computerChoice;
-    //console.log(randomNumber)
-    //console.log(computerChoice);
+function generateComputerChoice() {
+  const randomNumber = Math.floor(Math.random() * possibleChoices.length); // or * 3
+  computerChoice = choices[randomNumber];
+  computerChoiceDisplay.innerHTML = computerChoice;
+  //console.log(randomNumber)
+  //console.log(computerChoice);
 }
 
-function getResult(){
-    
+function setResult(){
+    numberResult = getResult();
+    if(numberResult == 0) userPoints +=1;
+    if(numberResult == 1) computerPoints +=1;
+    result =  results[numberResult];
+    console.log(result);
+    resultDisplay.innerHTML = result;
+}
+
+function getResult() {
+  if (userChoice === computerChoice) {
+    return 2;
+  } else {
+    switch (userChoice) {
+      case "rock":
+        if (computerChoice === "paper") return 1;
+        if (computerChoice === "scissors") return 0;
+        break;
+      case "paper":
+        if (computerChoice === "rock") return 0;
+        if (computerChoice === "scissors") return 1;
+        break;
+      case "scissors":
+        if (computerChoice === "paper") return 0;
+        if (computerChoice === "rock") return 1;
+        break;
+    }
+  }
 }
